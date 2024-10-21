d5462d6377f947a1fd3d45c2d8039a329ed8ef05
devcui <devcui@outlook.com>
2024-10-21 10:35:23 +0800

    feat: add canvas

--- pages/index.vue
+++ pages/index.vue
@@ -25,9 +25,18 @@
         </template>
       </Card>
     </div>
-    <div class="col-span-2 min-h-20">
-      <Card class="min-h-full">
+    <div class="col-span-2 flex flex-col">
+      <Card class="flex-grow">
         <template #content>
+          <ProgressSpinner v-if="loading"></ProgressSpinner>
+          <canvas ref="canvasRef" class="w-full h-full"></canvas>
+        </template>
+      </Card>
+
+      <Card class="mt-4" style="background-color: #f0f0f0;">
+        <template #content>
+          <div v-if="loading">loading...</div>
+          <div v-if="!loading" class="p-2 text-sm">识别到的文本: {{ ocrText }}</div>
         </template>
       </Card>
     </div>
@@ -36,23 +45,60 @@
 
 <script lang="ts" setup>
 import { useMyImageStore } from '~/stores/image';
+import { ref, type VNodeRef } from 'vue';
 
-const store = useMyImageStore()
 definePageMeta({
   layout: 'default',
 })
 
+const store = useMyImageStore()
+const loading = ref(false)
+const ocrResult = ref<any>(null);
+const ocrText = ref("");
+const imageSize = ref({ width: 800, height: 600 });
+const canvasRef = ref<HTMLCanvasElement | undefined>();
+
+const drawOcrResultOnCanvas = () => {
+  const canvas = canvasRef.value;
+  if (!canvas || !ocrResult.value || !ocrResult.value.data || !imageSize.value.width || !imageSize.value.height) return;
+  const ctx = canvas?.getContext('2d');
+  if (!ctx) return;
+  canvas.width = imageSize.value.width;
+  canvas.height = imageSize.value.height;
+  ctx.fillStyle = 'white';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ocrResult.value.data.forEach((item: any) => {
+    const [coordinates, text] = item;
+    const [topLeft, , bottomLeft] = coordinates;
+    const rectHeight = bottomLeft[1] - topLeft[1];
+    const fontSize = rectHeight * 0.8;
+    ctx.fillStyle = 'black';
+    ctx.font = `${fontSize}px Arial`;
+    const textX = topLeft[0];
+    const textY = topLeft[1] + rectHeight * 0.8;
+    ctx.fillText(text[0], textX, textY);
+  });
+  const combinedText = ocrResult?.value.data?.map((item: any) => item[1][0]).join(' ');
+  ocrText.value = combinedText
+  loading.value = false
+};
+
+
 
 
 const checkImage = async (image: ImageMeta) => {
+  loading.value = true
   if (image.type === 'upload' || image.type === 'base64') {
-    const data = await useLazyFetch('/ocr/predict-by-base64', { method: 'post', body: { base64_str: image.data } })
-    console.log(data.data.value)
+    const { data } = await useLazyFetch<any>('/ocr/predict-by-base64', { method: 'post', body: { base64_str: image.data } })
+    ocrResult.value = unref(data)
+    drawOcrResultOnCanvas()
   }
   if (image.type === 'internet') {
-    const data = await useLazyFetch(`/ocr/predict-by-url?imageUrl=${image.data}`)
-    console.log(data.data.value)
+    const { data } = await useLazyFetch<any>(`/ocr/predict-by-url?imageUrl=${image.data}`)
+    ocrResult.value = unref(data)
+    drawOcrResultOnCanvas()
   }
 }
+
 
 </script>
